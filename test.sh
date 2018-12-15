bd3b2390229508b1d404dd10f6a06ffca2d606be
jeremy <jeremy.f.buss@gmail.com>
2018-12-15 07:36:43 -0600

    update test for "assume-yes"

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 apt-get update
-apt-get install git
+apt-get --assume-yes install git
 git clone https://github.com/j-buss/lstm_nietzche.git
 cd lstm_nietzche/
 python3 cpu_perf_test.py

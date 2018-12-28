e25fa79fe0577f0220237addc3a6c3ffc5813894
jeremy <jeremy.f.buss@gmail.com>
2018-12-28 09:57:50 -0600

    Updated file names

--- create_stackdriver_policy.sh
+++ create_stackdriver_policy.sh
@@ -13,6 +13,6 @@ else
   	--header "$AUTHORIZATION_STRING$TOKEN" \
   	--header 'Accept: application/json' \
   	--header 'Content-Type: application/json' \
-  	--data '{"displayName":"Low CPU Warning","combiner":"OR","conditions":[{"displayName":"CPU Utilization is Low","conditionThreshold":{"aggregations":[{"alignmentPeriod":"60s","crossSeriesReducer":"REDUCE_MEAN","groupByFields":["project","resource.label.instance_id","resource.label.zone"],"perSeriesAligner":"ALIGN_MAX"}],"comparison":"COMPARISON_LT","duration":"120s","filter":"metric.type=\"compute.googleapis.com/instance/cpu/utilization\" AND resource.type=\"gce_instance\"","thresholdValue":0.09,"trigger":{"count":1}}}]}' \
+  	--data '{"displayName":"Low CPU Warning","combiner":"OR","conditions":[{"displayName":"CPU Utilization is Low","conditionThreshold":{"aggregations":[{"alignmentPeriod":"60s","crossSeriesReducer":"REDUCE_MEAN","groupByFields":["project","resource.label.instance_id","resource.label.zone"],"perSeriesAligner":"ALIGN_MAX"}],"comparison":"COMPARISON_LT","duration":"600s","filter":"metric.type=\"compute.googleapis.com/instance/cpu/utilization\" AND resource.type=\"gce_instance\"","thresholdValue":0.09,"trigger":{"count":1}}}]}' \
   	--compressed
 fi	
--- startup_script_lstm_nietzche.sh
+++ startup_script_lstm_nietzche.sh
@@ -4,8 +4,8 @@ trap 'exec 2>&4 1>&3' 0 1 2 3
 exec 1>log.out 2>&1
 # Everything below will go to the file 'log.out':
 sudo apt-get update
-sudo apt-get install git
-sudo apt-get install python3-pip
+sudo apt-get --yes --force-yes install git
+sudo apt-get --yes --force-yes install python3-pip
 git clone https://github.com/j-buss/lstm_nietzche.git
 cd lstm_nietzche/
 pip3 install -r requirements.txt
